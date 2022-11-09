2f5f1e2148e194a1412084925d3fcfe36a569bce
sank4lp55 <Sankalp2015415@akgec.ac.in>
2022-11-09 22:48:54 +0530

    mod

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '  Wallet module',
+      title: 'wallet mod',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
