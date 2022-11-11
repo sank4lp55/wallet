d0aaa50be526ea97e7811bf19e480225da4dae43
sank4lp55 <Sankalp2015415@akgec.ac.in>
2022-11-12 03:02:44 +0530

    wallet structure

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WalletView(),
+      home: const WalletPage(),
       debugShowCheckedModeBanner: false,
     );
   }
